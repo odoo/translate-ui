e449f595658b9c8d4f17e553a04f297950eb5f3c
Julien Mougenot <jum@odoo.com>
2025-12-10 16:32:47 +0100

    [FIX] make Python not crash

--- translate_mode/tools/translate.py
+++ translate_mode/tools/translate.py
@@ -2,95 +2,99 @@
 
 from __future__ import annotations
 
-from odoo.tools.translate import get_translation
+import functools
+import logging
+
+import odoo.tools.translate as translate
 
 
+_logger = logging.getLogger(__name__)
+
+
+original_get_translation = translate.get_translation
+
+
+def get_text_alias(source: str, /, *args, **kwargs) -> TranslatedString:
+    module, lang = translate._get_translation_source(1)
+    translation = get_translation(module, lang, source, args or kwargs)
+    return TranslatedString(translation, source, module)
+
+
+def get_translation(module: str, lang: str, source: str, args: tuple | dict) -> TranslatedString:
+    translation = original_get_translation(module, lang, source, args)
+    return TranslatedString(translation, source, module)
+
+
+def LazyGettext_translate(self, lang: str = '') -> str:
+    module, lang = translate._get_translation_source(2, self._module, lang, default_lang=self._default_lang)
+    return get_translation(module, lang, self._source, self._args)
+
+
+@functools.total_ordering
 class TranslatedString(str):
     __slots__ = '_evaluated_str', 'context', 'source', 'translation'
 
-    def __new__(cls, translation: str, source: str, context: str, *args, **kwargs):
-        return super().__new__(cls, translation, *args, **kwargs)
+    def __new__(cls, translation: str | TranslatedString, source: str, context: str):
+        return super().__new__(cls, str(translation))
 
     def __init__(self, translation: str | TranslatedString, source: str, context: str):
         if isinstance(translation, TranslatedString):
             self.translation = translation.translation
             if not context:
                 context = translation.context
+            if not source:
+                source = translation.source
         else:
             self.translation = translation
         self.context = context
         self.source = source
         self._evaluated_str = ""
 
-    def __add__(self, other):
-        if isinstance(other, TranslatedString):
-            translation = self.translation + other.translation
-            source = self.source + other.source
-            return TranslatedString(translation, source, self.context)
-        else:
-            return self.translation.__add__(other)
-
-    def __reduce__(self):
-        return (TranslatedString, (self.translation, self.source, self.context))
-
-    def __str__(self):
+    def _format(self):
         if not self._evaluated_str:
             translated = self.translation != self.source
-            self._evaluated_str = f"_({self.context},{int(translated)}{{{self.source}}}[{self.translation}])"
+            source = self.source.replace('%s', '%%s')
+            self._evaluated_str = f"_({self.context},{int(translated)}{{{source}}}[{self.translation}])"
         return self._evaluated_str
 
-    def __contains__(self, *args, **kwargs):
-        return self.translation.__contains__(*args, **kwargs)
+    def __repr__(self):
+        """ Show for the debugger"""
+        args = {'context': self.context, 'source': self.source, 'translation': self.translation}
+        return f"TranslatedString({args!r})"
 
-    def __eq__(self, *args, **kwargs):
-        return self.translation.__eq__(*args, **kwargs)
+    def __str__(self):
+        return self._format()
 
-    def __ge__(self, *args, **kwargs):
-        return self.translation.__ge__(*args, **kwargs)
+    def __eq__(self, other):
+        if not isinstance(other, TranslatedString):
+            return False
+        return (self.source == other.source and
+                self.translation == other.translation and
+                self.context == other.context)
 
-    def __getitem__(self, *args, **kwargs):
-        return self.translation.__getitem__(*args, **kwargs)
+    def __hash__(self):
+        return self._format().__hash__()
 
-    def __gt__(self, *args, **kwargs):
-        return self.translation.__gt__(*args, **kwargs)
+    def __lt__(self, other):
+        return NotImplemented
 
-    def __hash__(self, *args, **kwargs):
-        return self.translation.__hash__(*args, **kwargs)
+    def __add__(self, other):
+        if isinstance(other, str):
+            return self._format() + other
+        elif isinstance(other, TranslatedString):
+            return self._format() + other._format()
+        return NotImplemented
 
-    def __iter__(self, *args, **kwargs):
-        return self.translation.__iter__(*args, **kwargs)
+    def __radd__(self, other):
+        if isinstance(other, str):
+            return other + self._format()
+        return NotImplemented
 
-    def __le__(self, *args, **kwargs):
-        return self.translation.__le__(*args, **kwargs)
-
-    def __len__(self, *args, **kwargs):
-        return self.translation.__len__(*args, **kwargs)
-
-    def __lt__(self, *args, **kwargs):
-        return self.translation.__lt__(*args, **kwargs)
-
-    def __mod__(self, *args, **kwargs):
-        return self.translation.__mod__(*args, **kwargs)
-
-    def __mul__(self, *args, **kwargs):
-        return self.translation.__mul__(*args, **kwargs)
-
-    def __ne__(self, *args, **kwargs):
-        return self.translation.__ne__(*args, **kwargs)
-
-    def __repr__(self, *args, **kwargs):
-        return self.translation.__repr__(*args, **kwargs)
-
-    def __rmod__(self, *args, **kwargs):
-        return self.translation.__rmod__(*args, **kwargs)
-
-    def __rmul__(self, *args, **kwargs):
-        return self.translation.__rmul__(*args, **kwargs)
-
-    def __sizeof__(self, *args, **kwargs):
-        return self.translation.__sizeof__(*args, **kwargs)
+    def __mod__(self, other):
+        return self._format().__mod__(other)
 
 
-def _get_translation(module: str, lang: str, source: str, args: tuple | dict) -> str:
-    translation = get_translation(module, lang, source, args)
-    return TranslatedString(translation, source, module)
+translate._ = get_text_alias
+translate.get_text_alias = get_text_alias
+translate.get_translation = get_translation
+translate.LazyGettext._translate = LazyGettext_translate
